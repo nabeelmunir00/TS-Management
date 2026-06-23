a63f1e00ff49ba136813edc590add7ae2ec57c78
Nabeel Munir <nabeelgaming786yt@gmail.com>
2026-06-23 18:57:48 +0500

    feat(layout): integrate JetBrains Mono font for improved typography in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import { ClerkProvider } from "@clerk/nextjs";
 // import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Geist, JetBrains_Mono } from "next/font/google";
 
 // const inter = Inter({ subsets: ["latin"] });
+const mono = JetBrains_Mono({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Developer Hub - Task Management System",
@@ -18,7 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={mono.className}>
           {children}
           <Toaster position={"top-right"} />
         </body>
